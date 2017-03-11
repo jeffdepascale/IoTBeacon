82e262eb276a2b5acf8055f9df398a2260106989
jeffdepascale <jdepascale@testpilot111.com>
2017-03-11 18:32:06 -0500

    Moved to class based model

--- beacon.py
+++ beacon.py
@@ -9,74 +9,97 @@ import pygame
 import logging
 import sys
 import string
- 
-led = LED(17)
-failConnectCount = 0
+
+
 logging.basicConfig(level=logging.DEBUG, filename="/home/pi/share/launch", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-						
+								format="%(asctime)-15s %(levelname)-8s %(message)s")
+								
 
 def global_except_hook(exctype, value, traceback):
-	logging.error(value);
-	if str(value).find("Adafruit") != -1:
-		reconnect()
-		pass
-	else:
-		sys.__excepthook__(exctype, value, traceback)
-
+		logging.error(value);
+		if str(value).find("Adafruit") != -1:
+			connectState = ConnectState.PendingReconnect
+			pass
+		else:
+			sys.__excepthook__(exctype, value, traceback)
+				
 sys.excepthook = global_except_hook
 
-def connected(client):
-	logging.info("connected")
-	global failConnectCount
+
+class ConnectState:
+    Disconnected, Connecting, Connected, PendingReconnect = range(4)
+
+	
+	
+	
+class Beacon(object):
+
+	led = LED(17)	
+	connectState = ConnectState.Disconnected
 	failConnectCount = 0
-	client.subscribe('cameraevent') # or change to whatever name you used	
-
-def message(client, feed_id, payload):
-	msgStr = 'Feed {0} received new value: {1}'.format(feed_id, payload)
-	log_data = ""
-	with open('/home/pi/share/launch', 'r') as myfile:
-		log_data=myfile.read().replace('\n', '')
-	if log_data.find(msgStr) == -1:
-		logging.info(msgStr)
-		mixer.init()
-		mixer.music.load('/home/pi/share/notification.mp3')
-		mixer.music.set_volume(1)
-		mixer.music.play()
-		led.on()
-		sleep(5)
-		led.off()
-		mixer.quit()
-
-def disconnected(client):
-	logging.info('Disconnected from AdafruitIO')
-	reconnect()
-
-def connect():
-	try:
-		client.connect()
-	except AdafruitIOError as e:
-		logging.error(e)
-		reconnect()
+	client = None
 	
-
-def reconnect():
-	global failConnectCount
-	failConnectCount += 1
-	logging.info('pending reconnect - failcount=' + str(failConnectCount))
-	sleep(10)
-	connect()
-	
-led.on()
-sleep(1)
-led.off()		
+	def __init__(self):
+		logging.info("Beacon service initialized")
 		
-client = MQTTClient('Jdepascale', 'ee0b91b182134253987e863af9be67ac')
+		self.led.on()
+		sleep(1)
+		self.led.off()
+		
+		self.client = MQTTClient('Jdepascale', 'ee0b91b182134253987e863af9be67ac')
 
-# Setup the callback functions defined above.
-client.on_connect    = connected
-client.on_disconnect = disconnected
-client.on_message    = message
+		self.client.on_connect    = self.connected
+		self.client.on_disconnect = self.disconnected
+		self.client.on_message    = self.message	
+		
+		while True:
+			if self.connectState == ConnectState.Disconnected:
+				self.connect()
+			elif self.connectState == ConnectState.PendingReconnect:
+				self.reconnect()
+			self.client.loop()
+	
+	def message(self, client, feed_id, payload):
+		msgStr = 'Feed {0} received new value: {1}'.format(feed_id, payload)
+		log_data = ""
+		with open('/home/pi/share/launch', 'r') as myfile:
+			log_data=myfile.read().replace('\n', '')
+		if log_data.find(msgStr) == -1:
+			logging.info(msgStr)
+			mixer.init()
+			mixer.music.load('/home/pi/share/notification.mp3')
+			mixer.music.set_volume(1)
+			mixer.music.play()
+			self.led.on()
+			sleep(5)
+			self.led.off()
+			mixer.quit()
 
-connect()
-client.loop_blocking()
+	def connected(self, client):
+		logging.info("connected")
+		self.connectState = ConnectState.Connected
+		self.failConnectCount = 0
+		self.client.subscribe('cameraevent') # or change to whatever name you used	
+				
+	def disconnected(self, client):
+		logging.info('Disconnected from AdafruitIO')
+		self.connectState = ConnectState.Disconnected
+		self.reconnect();
+	
+	def connect(self):
+		logging.info("connect")
+		self.connectState = ConnectState.Connecting
+		try:
+			self.client.connect()
+		except AdafruitIOError as e:
+			logging.error(e)
+			self.reconnect()
+
+	def reconnect(self):
+		self.failConnectCount += 1
+		logging.info('pending reconnect - failcount=' + str(self.failConnectCount))
+		self.connectState = ConnectState.Connecting
+		sleep(10)
+		self.connect()
+	
+beacon = Beacon()		
